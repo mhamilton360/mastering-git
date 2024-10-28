b106c497e02ff42fb498809eaec4ef85635c59aa
mhamilton360 <mhamilton360@gmail.com>
2024-10-27 20:55:12 -0400

    Add employees list to manage employees

--- src/main/java/com/ims/Main.java
+++ src/main/java/com/ims/Main.java
@@ -1,10 +1,15 @@
 package com.ims;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Mike", 65);
+        List<Employee> employees = Arrays.asList(employee, new Employee("Pete", 55));
 
         System.out.println(employee);
         System.out.println("=========================");
@@ -13,5 +18,9 @@ public class Main {
             if(i > 1) System.out.print(", ");
             System.out.print("i = " + i);
         }
+        System.out.println("\n=========================");
+
+        employees.forEach(System.out::println);
+        System.out.println("\n=========================");
     }
 }
